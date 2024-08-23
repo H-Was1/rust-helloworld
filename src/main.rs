c00f03c913c2110e5563d63bd7875290d0813e4e
wasi <hammadwasi.b@gmail.com>
2024-08-23 12:53:46 +0500

    control flow practice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("hi rustling rust is great")
+    let mut a = 4;
+
+    //  loop loop
+
+    'nloop: loop {
+        println!("{}", a);
+        a -= 1;
+        if a == 0 {
+            break;
+        }
+    }
+
+    // let loop
+
+    let x = loop {
+        println!("{}", a);
+
+        a += 1;
+        if a == 10 {
+            break a;
+        }
+    };
+
+    // while loop
+
+    while a != 0 {
+        println!("{}", a);
+        a -= 1;
+    }
+
+    // for loop
+    // println!("{}", a);
+
 }
