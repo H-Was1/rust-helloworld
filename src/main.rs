6ffac6438aee27f40e7ceca97d23ac859a2b0495
wasi <hammadwasi.b@gmail.com>
2024-09-15 01:03:04 +0500

    FIX

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
 mod modules;
 fn main() {
-    let tweet = String::from("rust bootcamp");
-    let ans = first_word(&tweet);
-    println!("First word: {}", ans);
-}
-
-fn first_word(st: &str) -> &str {
-    // split the &str at first space and return
-    &st[..st.find(' ').unwrap_or(st.len())]
+    
 }
